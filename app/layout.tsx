b1a22bc86ea0566bdee1a7c963be77b671626f8c
scorpion77777 <vvmartirosyan@gmail.com>
2025-06-29 15:02:54 +0400

    Add README

---
+++ app/layout.tsx
@@ -0,0 +1,18 @@
+import "@/app/ui/global.css";
+import { inter } from "@/app/ui/fonts";
+import { Analytics } from '@vercel/analytics/next';
+
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={`${inter.className} antialiased`}>{children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
