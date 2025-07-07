9f5f01e042e8411832f6fed6996a15a7143443f9
Vahan Martirosyan <vvmartirosyan@gmail.com>
2025-07-07 21:13:55 +0400

    Forth commit

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Page() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           <Image
           priority={false} 
-            src="/hero-desktop.png"
+            src="/cell.jpg"
             width={1000}
             height={760}
             className="hidden md:block"
